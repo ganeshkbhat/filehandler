c5016d9f7696b5b8de62bbfd43e56d77c13ca7da
Ganesh B <ganeshsurfs@gmail.com>
2024-02-15 10:44:20 +0530

    [COMMIT] Adding stream-fs package implementation for jpeg handler

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+/**
+ *
+ * Package: fs-handlers
+ * Author: Ganesh B
+ * Description: Read and Write files in files using streaming
+ * Install: npm i fs-handlers --save
+ * Github: https://github.com/ganeshkbhat/filehandler
+ * npmjs Link: https://www.npmjs.com/package/fs-handlers
+ * File: index.js
+ * File Description: Read and Write files in files using streaming
+ *
+ *
+*/
+export { FileHandlerInterface, FileHandlerStaticInterface } from "./src/filehandler";
+export { FileHandler, FileHandlerStatic } from "./src/filehandler";
+declare const _default: {
+    FileHandler: typeof import("./src/filehandler").FileHandler;
+    FileHandlerStatic: typeof import("./src/filehandler").FileHandlerStatic;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
